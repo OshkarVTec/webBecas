965fbed121bdc1d8eb6ddb37397d0db0d6049104
OshkarVTec <a01275287@tec.mx>
2023-11-27 16:38:54 -0600

    change logic of filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
 	const becas = data.becas;
 	const [becasFinal, setBecasFinal] = useState(new Set());
 	// Añadir todos los datos de los filtros
-	let nameSearch = "";
+
 	const categorias = new Map();
 	const edades = new Map();
 	const nivelesEducativos = new Map();
@@ -17,42 +17,58 @@ function App() {
 		addItems(beca.edad, edades);
 		addItems(beca.nivelEducativo, nivelesEducativos);
 	});
+	const [nameSearch, setNameSearch] = useState("");
 	//Poner nombre a los filtros
-	const filtros = new Map();
-	filtros.set("categoria", categorias);
-	filtros.set("edad", edades);
-	filtros.set("nivelEducativo", nivelesEducativos);
+	const filtrosTemp = new Map();
+	filtrosTemp.set("categoria", categorias);
+	filtrosTemp.set("edad", edades);
+	filtrosTemp.set("nivelEducativo", nivelesEducativos);
+	const [filtros, setFiltros] = useState(filtrosTemp);
+
+	useEffect(() => filtrar(), [filtros, nameSearch]);
 
 	function addItems(currentValue, map) {
 		if (Array.isArray(currentValue)) {
 			for (let i of currentValue) {
-				map[i] = false;
+				map.set(i, false);
 			}
 		} else {
-			map[currentValue] = false;
+			map.set(currentValue, false);
 		}
 	}
 
 	function isIn(currentValue, filterMap) {
 		if (Array.isArray(currentValue)) {
 			for (let i of currentValue) {
-				if (filterMap[i]) return true;
+				if (filterMap.get(i)) return true;
 			}
 		} else {
-			if (filterMap[currentValue]) return true;
+			if (filterMap.get(currentValue)) return true;
 		}
 		return false;
 	}
-
 	function filtrar() {
+		let tempStorage = becas;
 		let currentBecas = new Set();
 		let filterFlag = false;
-		for (let [filterName, filterSet] of filtros) {
-			becas
-				.filter((i) => isIn(i[filterName], filterSet))
-				.forEach((item) => currentBecas.add(item));
-			if (filterSet.size > 0) filterFlag = true;
+		let secondFlag = false;
+		for (let [filterName, filterMap] of filtros) {
+			currentBecas = new Set();
+			secondFlag = false;
+			for(let [_key, value] of filterMap ){
+				if(value) secondFlag = true;
+			}
+			if(secondFlag){
+				tempStorage
+				.filter((i) => isIn(i[filterName], filterMap))
+				.forEach((item) => {
+					currentBecas.add(item);
+					if (filterMap.size > 0) filterFlag = true;
+				});
+			tempStorage = Array.from(currentBecas);
+			}
 		}
+		currentBecas = new Set(tempStorage);
 		if (!filterFlag) {
 			currentBecas = new Set(becas);
 		}
@@ -71,18 +87,38 @@ function App() {
 			<div>
 				<form onSubmit={(e) => e.preventDefault()}>
 					<input
+						value={nameSearch}
 						type="text"
 						placeholder="Buscar"
 						onChange={(e) => {
-							nameSearch = e.target.value.toLowerCase();
-							filtrar();
+							setNameSearch(e.target.value.toLowerCase());
 						}}
 					></input>
+					{/*Filtros*/}
+					{Array.from(filtros.keys()).map((nombreFiltro) => (
+						<div key={nombreFiltro}>
+							{Array.from(filtros.get(nombreFiltro).keys()).map((key) => (
+								<label key={key}>
+									<input
+										key={key}
+										id={key}
+										type="checkbox"
+										name={key}
+										value={filtros.get(nombreFiltro).get(key)}
+										onChange={(e) => {
+											filtros.get(nombreFiltro).set(key, e.target.checked);
+											setFiltros(new Map(filtros));
+										}}
+									/>
+									{key}
+								</label>
+							))}
+						</div>
+					))}
 				</form>
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => filtrar()}>Resultados</button>
 				<p>{becasFinal.size}</p>
 			</div>
 			<p className="read-thttps://stackoverflow.com/questions/74584926/filtering-with-checkboxes-in-reacthe-docs">
